526fb6af4cb70c03fba07955dda24d12dd8e546d
5undel <max.sundel@Choice.se>
2022-03-06 14:21:41 +0000

    Respell allowes host name

--- lgm/settings.py
+++ lgm/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["let-get-moving.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["lets-get-moving.herokuapp.com", "localhost"]
 
 
 # Application definition
