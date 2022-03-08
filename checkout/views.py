31dcd55ffc04260ee9a639c01bc6c29699b0040d
5undel <max.sundel@Choice.se>
2022-03-08 20:41:38 +0000

    Added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Kam1sBZywz1S249sDKthLEziMimVdDqC5Dc9u8J3FrU205CISNTJJeQN87UOJLbqa59ok1EtG2iY5hHzkowNt1600OQ9zjTgm',
+        'client_secret': 'test client secret',
     }
 
     return render(request, 'checkout/checkout.html', context)
