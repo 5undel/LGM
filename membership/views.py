7a966a3dfae99e5103286ee71237a2d8eced66c3
5undel <max.sundel@Choice.se>
2022-03-08 10:11:06 +0000

    Added product details functionality

--- membership/views.py
+++ membership/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
 
 
 
 # Create your views here.
 def all_products(request):
-    """ A view to return the index page """
+    """ A view to return the membership page """
 
     products = Product.objects.all()
 
@@ -14,5 +14,17 @@ def all_products(request):
     }
 
     return render(request, 'membership/membership.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to return the membership detail page """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product' : product,
+    }
+
+    return render(request, 'membership/product_detail.html', context)
 
 
