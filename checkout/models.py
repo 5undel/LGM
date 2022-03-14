3d9bbda822d046abddcbeaecb400e7925feffa69
5undel <max.sundel@Choice.se>
2022-03-14 17:42:20 +0000

    Add user_profile to model

--- checkout/models.py
+++ checkout/models.py
@@ -5,11 +5,14 @@ from django.db.models import Sum
 from django.conf import settings
 
 from membership.models import Product
+from profiles.models import UserProfile
 
 # Create your models here.
 
 class CreateMembership(models.Model):
     membership_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
