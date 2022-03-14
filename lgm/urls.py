103c0109151f551ef9a2e50a13827888c0daa860
5undel <max.sundel@Choice.se>
2022-03-14 17:43:29 +0000

    Added path to new app

--- lgm/urls.py
+++ lgm/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('base.urls')),
     path('membership/', include('membership.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
