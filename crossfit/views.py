ab8e1b10ba290497bf1eb4f206176a11816d9ff5
5undel <max.sundel@Choice.se>
2022-03-15 17:48:05 +0000

    Add new app crosssfit

---
+++ crossfit/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def all_products(request):
+    """ A view to return the membership page """
+
+    
+
+    context = {}
+
+    return render(request, 'crossfit/crossfit.html', context)
