a6194f7c9fd3cd65c98b57c1a9fd6c8b80f34b65
5undel <max.sundel@Choice.se>
2022-03-25 09:44:09 +0000

    Add Contact page and link to email

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-#url path to the home page
+
+
+# url path to the home page
 urlpatterns = [
     path('', views.index, name='home'),
-    path('userlogin', views.userlogin, name='userlogin')
-
+    path('userlogin', views.userlogin, name='userlogin'),
+    path('contact', views.contact, name='contact'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'base/index.html')
 
+
 def userlogin(request):
     """ A view to return the user login page """
 
     return render(request, 'base/userlogin.html')
+
+def contact(request):
+    """ A view to return the contact page """
+
+    return render(request, 'base/contact.html')
