1abc58bf9a8d4f1d185089822c4ed52cd88e64d4
5undel <max.sundel@Choice.se>
2022-03-25 07:02:56 +0000

    Add send email function

--- profiles/views.py
+++ profiles/views.py
@@ -12,15 +12,21 @@ from checkout.models import CreateMembership
 @login_required
 def profile(request):
     """ Display the user's profile. """
-
-    
     profile = get_object_or_404(UserProfile, user=request.user)
+    
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'orders': orders,
         'from_profile': True,
+        'profile': profile,
 
     }
 
