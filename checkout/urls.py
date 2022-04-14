63a6ef182cc57a08ec9dc67dce018c4569f7c3b2
5undel <max.sundel@Choice.se>
2022-04-14 07:39:29 +0000

    Add webhook.py

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 # path for the checkout and success page
 urlpatterns = [
     path('<int:pk>/', views.checkout, name='checkout'),
     path('checkout_success/<membership_number>', views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
