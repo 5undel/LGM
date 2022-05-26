2867500e7f2a3a0158024480c76ec8118ae37079
5undel <max.sundel@Choice.se>
2022-05-26 13:33:44 +0000

    Change debug to true

--- lgm/settings.py
+++ lgm/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ["lets-get-moving.herokuapp.com", "localhost"]
 
